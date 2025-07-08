c07f6bf4301536edb7680a2c52cab856a20c91ba
Bruno da Silva James <136830934+Bruno-da-Silva-james@users.noreply.github.com>
2025-07-08 10:41:38 -0300

    0

--- Study/React/myapp/src/App.js
+++ Study/React/myapp/src/App.js
@@ -1,15 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function MyButton() {
-  return <button>test</button>;
+function HeaderButton(letraBotao) {
+  return <button id="HeaderButton">teste</button>;
 }
 
 function Test() {
   return (
     <div>
       <h1>Welcome to my app</h1>
-      <MyButton />
     </div>
   );
 }
@@ -30,6 +29,10 @@ function Card() {
 export default function Main() {
   return (
     <div className="App">
+      <div className="Header">
+        <a1>Bruno da Silva James</a1>
+        <HeaderButton></HeaderButton>
+      </div>
       <Test />
       <Card></Card>
     </div>
