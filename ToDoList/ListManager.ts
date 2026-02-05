4a726e242e4bef692b22acd1dbbdece845e5417d
Bruno da Silva James <136830934+Bruno-da-Silva-james@users.noreply.github.com>
2026-02-04 23:23:43 -0300

    Firebase Implementation

---
+++ ToDoList/ListManager.ts
@@ -0,0 +1,13 @@
+import { DATABASE } from "./Firebase";
+
+class ListManager {
+  constructor() {}
+
+  addTask() {}
+
+  removeTask() {}
+
+  updateTask() {}
+
+  getTasks() {}
+}
--- ToDoList/ToDoList.ts
+++
@@ -1,6 +0,0 @@
-let taskList: 
-
-
-void function addTask(){
-    
-}
